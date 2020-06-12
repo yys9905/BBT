c0a600526250ba0fa1fe6e5d6cf887e6b51314c4
jiyeon0116 <gbko159@naver.com>
2020-06-12 11:07:47 +0900

    merge

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -1,7 +1,8 @@
 package Main;
-
+import java.util.Scanner;
 
 /**
+ * calculator
  * 
  * 
  * @author (2018315036 양유석,2018315003 최세원, 2018315022 이지연) 
@@ -9,5 +10,12 @@ package Main;
  */
 public class MyApp
 {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        int old = sc.nextInt();
+        boolean past = true;
+        Calculation.Calculator c = new Calculation.Calculator();
+        c.Calculator(age, past);
+    }
 }
