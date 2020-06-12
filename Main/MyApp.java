824ee61abc2b8c0dc15d43cc7e631088ba2c26f0
FWSU <yys9905@naver.com>
2020-06-12 11:05:04 +0900

    add scaneer and make instance of Calculator class

--- Main/MyApp.java
+++ Main/MyApp.java
@@ -1,13 +1,20 @@
 package Main;
-
+import java.util.Scanner;
 
 /**
- * 여기에 MyApp 클래스 설명을 작성하십시오.
+ * calculator
  * 
  * @author (작성자 이름) 
  * @version (버전번호나 날짜)
  */
 public class MyApp
 {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+        int old = sc.nextInt();
+        boolean past = true;
+        Calculation.Calculator c = new Calculation.Calculator();
+        c.Calculator(age, past);
+    }
 }
